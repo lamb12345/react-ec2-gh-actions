6acaefaf3bd74495292954bb142e37aa6400b6ef
Lambertn33 <lambertgithub@gmail.com>
2023-10-09 16:12:42 +0200

    feat: display users

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { Users } from "./components/Users";
+
+const App = () => {
+  return (
+    <div className="h-screen w-screen bg-slate-300">
+      <div className="w-full h-full border">
+        <div className="flex justify-start items-center flex-col h-full gap-8">
+          <h2 className="text-4xl underline">Users List</h2>
+            <Users />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+export default App;
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,5 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export const Users = () => {
-  return <div>Users</div>;
+  const [usersData, setUsersData] = useState({
+    users: [],
+    isFetching: false,
+  });
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      setUsersData((prevState) => {
+        return { ...prevState, isFetching: true };
+      });
+      const response = await axios.get(process.env.REACT_APP_BACKEND_URL);
+      setUsersData({
+        users: response.data,
+        isFetching: false,
+      });
+    };
+    fetchUsers();
+  }, []);
+
+  const RenderUser = ({ user }) => {
+    return (
+      <div className="border-b py-3">
+        <h2>{user.name}</h2>
+      </div>
+    );
+  };
+  
+  return (
+    <div className="bg-white w-1/2 p-8 rounded-md">
+      {usersData.isFetching ? (
+        <h2>Please wait...</h2>
+      ) : (
+        usersData.users.map((user) => {
+          return <RenderUser key={user.id} user={user} />;
+        })
+      )}
+    </div>
+  );
 };
