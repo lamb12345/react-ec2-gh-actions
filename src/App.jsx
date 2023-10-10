a6fb12010e1f8c7ce12f1e76422db0240c49abdc
Lambertn33 <lambertgithub@gmail.com>
2023-10-10 11:15:20 +0200

    feat: change something

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
     <div className="h-screen w-screen bg-slate-300">
       <div className="w-full h-full border">
         <div className="flex justify-start items-center flex-col h-full gap-8">
-          <h2 className="text-4xl underline">Users List</h2>
+          <h2 className="text-4xl underline">Users List App</h2>
             <Users />
         </div>
       </div>
